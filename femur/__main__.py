aad834ffb92ea21710f65ca1ee63057c717dcb2b
deviantfero <fmorataya.04@gmail.com>
2018-06-04 01:04:55 -0600

    rename calc module as data module

--- femur/__main__.py
+++ femur/__main__.py
@@ -1,11 +1,11 @@
-from . import calc
+from . import data
 
 
 def main():
     k, q = [float(n) for n in input("input k and q: ").split()]
     xs = [float(x) for x in input("input x values: ").split()]
     ys = [float(y) for y in input("input y values: ").split()]
-    print(calc.local_fem(k, q, xs, ys))
+    print(data.local_fem(k, q, xs, ys))
 
 
 if __name__ == "__main__":
