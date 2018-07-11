29552df7eb682593a1d404491c64874c30ecb8d1
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-07-11 01:26:48 -0600

    removing noslip elements (bugged)

--- femur/navier_stokes_fem3d.py
+++ femur/navier_stokes_fem3d.py
@@ -1,8 +1,31 @@
 import sympy as sym
+import math
 import numpy as npy
 from . import matrices_nav_stokes as matrices
 from .symbols import *
 from sympy.interactive import printing
+
+# # force section
+# B = velocity * (matrices.Jdet) * (matrices.NT_integrated_respecto_to_epsilon *
+#                                   matrices.gradNxX)  # * matrices.velocity_arr
+# C = (-matrices.Jdet / rho) * (matrices.NT_integrated_respecto_to_epsilon *
+#                               matrices.gradNxX)  # * matrices.overlapped_pressure_arr
+# D = matrices.gradNxX_gradNxXt #* matrices.velocity_arr
+# E = (-matrices.Jdet) * (matrices.NT_integrated_respecto_to_epsilon *
+#                         matrices.gradNxX)  # * matrices.velocity_arr
+
+def is_on_plane(point, plane_eq):
+    return 0 - round(plane_eq.subs([(x, point.x), (y, point.y), (z, point.z)])) == round(0.0001, 3)
+
+def surface_from_three_points(point1, point2, point3):
+    vect_p1_p = [x - point1.x, y - point1.y, z - point1.z]
+    vect_p1_p2 = [point2.x - point1.x, point2.y - point1.y, point2.z - point1.z]
+    vect_p1_p3 = [point3.x - point1.x, point3.y - point1.y, point3.z - point1.z]
+
+    surface_eq = sym.Matrix([vect_p1_p, vect_p1_p2, vect_p1_p3])
+
+    return surface_eq.det()
+
 
 def gradient(num, dom):
     result = sym.Matrix([])
@@ -32,15 +55,6 @@ def overlap_matrix_row(matrix, times):
             result = result.row_insert(result.shape[0], matrix.row(row))
 
     return result
-
-# # force section
-# B = velocity * (matrices.Jdet) * (matrices.NT_integrated_respecto_to_epsilon *
-#                                   matrices.gradNxX)  # * matrices.velocity_arr
-# C = (-matrices.Jdet / rho) * (matrices.NT_integrated_respecto_to_epsilon *
-#                               matrices.gradNxX)  # * matrices.overlapped_pressure_arr
-# D = matrices.gradNxX_gradNxXt #* matrices.velocity_arr
-# E = (-matrices.Jdet) * (matrices.NT_integrated_respecto_to_epsilon *
-#                         matrices.gradNxX)  # * matrices.velocity_arr
 
 def variables_vector(cant):
     aux = []
@@ -80,7 +94,61 @@ def navier_stokes_local(
 def navier_stokes(data, force_arr):
     conn_size = len(data["nodes"])
 
-    #6 times cause i have 6 variables
+    #applying no slip conditions
+    conns_mat = data["connections"]
+    noslip_elems = data["noslip_elems"]
+    noslip_nodes = [x for x in data["nodes"] if x.is_noslip]
+
+
+    #finding plane and removing nodes that belong to plane (only for no slip nodes)
+    while len(noslip_elems) > 0:
+        print('noslip elements left: ', len(noslip_elems))
+        curr_elem = noslip_elems[0]
+        nodes_pos_in_elem = conns_mat[curr_elem]
+
+        #getting the node objects in an array
+        nodes_vals_in_elem = []
+        for i in nodes_pos_in_elem:
+            nodes_vals_in_elem += [data["nodes"][i]]
+
+        #noslip nodes for this iteration (with this i create the plane)
+        curr_noslip_nodes = [x for x in nodes_vals_in_elem if x.is_noslip]
+        plane = sym.Poly(surface_from_three_points(*curr_noslip_nodes))
+
+        #removing all nodes in plane
+        nodes_in_plane = [x for x in data["nodes"] if is_on_plane(x, plane)]
+        normal_vector = sym.Matrix([plane.coeffs()[:3]])
+        unit_vector = normal_vector / math.sqrt(normal_vector[0, 0]**2 + normal_vector[0, 1]**2 + normal_vector[0, 2]**2)
+        noslip_velocity_vector = unit_vector * 638
+
+        #removing all no_slip elements that contain this nodes
+        elem_match = []
+        cond = 0
+
+        for elem, i in enumerate(noslip_elems):
+            for node in elem:
+                cond += node in nodes_in_plane
+            elem_match[i] = cond == 1
+
+        new_elems = [noslip_elems[x] for x in range(len(noslip_elems)) if not elem_match[x]]
+        print('len of noslip_elems: ', len(noslip_elems))
+        print('len of new noslip elems: ', len(new_elems))
+        
+        break
+
+        # new_noslip_elems = []
+        # for elem in noslip_elems[1:]:
+        #     for node in data["connections"][elem]:
+        #         if data["nodes"][node] not in nodes_in_plane:
+        #             new_noslip_elems += [elem_match]
+        #             break
+
+        print('nodes in plane: ', len(nodes_in_plane))
+        noslip_elems = new_noslip_elems
+
+
+
+    #6 times because i have 6 variables
     assembly_left_mat = sym.Matrix(conn_size * 6, conn_size * 6, ([0] * ((conn_size * 6)**2)))
     variables_mat = variables_vector(conn_size)
     assembly_right_mat = sym.Matrix(conn_size * 6, 1, [0] * conn_size * 6)
@@ -128,11 +196,12 @@ def navier_stokes(data, force_arr):
     for i in range(len(data["nodes"])):
         if data["nodes"][i].is_input:
             dirchlet_position_array += [i]
+    dirchlet_position_array.sort()
 
     #applying dirchlet condition
-    velocity_0 = 15
+    velocity_0 = -638
 
-    #adding the velocity to right mat
+    #adding the initial velocity to right mat
     for i in range(len(dirchlet_position_array)):
         #adding to right matrix the vx and vy for each node in the entry face of the model
         assembly_right_mat[ : , 0] += assembly_left_mat[ : , dirchlet_position_array[i] * 6] * (-velocity_0)
@@ -177,6 +246,25 @@ def navier_stokes(data, force_arr):
     cols = [j for j in range(n) if any(assembly_right_mat[i, j] != rem for i in range(m))]
         
     assembly_right_mat = assembly_right_mat[rows, cols]
+
+    #applying no slip conditions
+    conns_mat = data["connections"]
+    noslip_elems = data["noslip_elems"]
+    noslip_nodes = [x for x in data["nodes"] if x.is_noslip]
+
+    #finding plane
+    while len(noslip_nodes) > 0:
+        curr_elem = noslip_elems[0]
+        nodes_pos_in_elem = conns_mat[curr_elem]
+
+        #getting the node objects in an array
+        nodes_vals_in_elem = []
+        for i in nodes_pos_in_elem:
+            nodes_vals_in_elem += [data["nodes"][i]]
+        #noslip nodes for this iteration (with this i create the plane)
+        curr_noslip_nodes = [x for x in nodes_vals_in_elem if x.is_noslip]
+        plane = sym.Poly(surface_from_three_points(*curr_noslip_nodes))
+
 
     print('K Matrix shape: \n', assembly_left_mat.shape)
     print('Vars Matrix shape: \n', variables_mat.shape)
