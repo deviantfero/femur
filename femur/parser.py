3ac8caeb1daa9786f562544d1eb7fbb756c5958d
deviantfero <fmorataya.04@gmail.com>
2018-07-10 22:04:33 -0600

    add new addition to parser

--- femur/parser.py
+++ femur/parser.py
@@ -26,6 +26,7 @@ def parse_info():
             data = [float(x) for x in line if x is not ""]
             if data and stage == 0:
                 problem_info["node_count"] = data[0]
+                problem_info["end_time"] = data[1]
                 problem_info["time_delta"] = data[2]
                 problem_info["velocity"] = data[3]
                 problem_info["density"] = data[4]
