541edfb62355a6babbbf27252537025ee91c803b
Alejandro Velasco <alejandrovelasco94@gmail.com>
2018-07-11 04:29:42 -0600

    implementation of time

--- femur/navier_stokes_fem3d.py
+++ femur/navier_stokes_fem3d.py
@@ -94,70 +94,16 @@ def navier_stokes_local(
 def navier_stokes(data, force_arr):
     conn_size = len(data["nodes"])
 
-    #applying no slip conditions
-    conns_mat = data["connections"]
-    noslip_elems = data["noslip_elems"]
-    noslip_connections = [data["connections"][x] for x in data["noslip_elems"]]
-    noslip_nodes = [x for x in data["nodes"] if x.is_noslip]
-
-
-    #finding plane and removing nodes that belong to plane (only for no slip nodes)
-    count = 0
-    while len(noslip_elems) > 0 and count < 12:
-        curr_elem = noslip_elems[0]
-        nodes_pos_in_elem = conns_mat[curr_elem]
-
-        #getting the node objects in an array
-        nodes_vals_in_elem = []
-        for i in nodes_pos_in_elem:
-            nodes_vals_in_elem += [data["nodes"][i]]
-
-        #noslip nodes for this iteration (with this i create the plane)
-        curr_noslip_nodes = [x for x in nodes_vals_in_elem if x.is_noslip]
-        plane = sym.Poly(surface_from_three_points(*curr_noslip_nodes))
-
-        #removing all nodes in plane
-        nodes_in_plane = [x for x in data["nodes"] if is_on_plane(x, plane)]
-        normal_vector = sym.Matrix([plane.coeffs()[:3]])
-        unit_vector = normal_vector / math.sqrt(normal_vector[0, 0]**2 + normal_vector[0, 1]**2 + normal_vector[0, 2]**2)
-        noslip_velocity_vector = unit_vector * 638
-
-        #removing all no_slip elements that contain this nodes
-        elem_match = []
-
-        for i, elem in enumerate(noslip_connections):
-            for node in elem:
-                cond = 0
-                for nip in nodes_in_plane:
-                    cond += data["nodes"][node] == nip
-            elem_match.append(cond > 0)
-
-        count += 1
-
-        new_elems = [noslip_elems[x] for x in range(len(noslip_elems)) if not elem_match[x]]
-        print('len of noslip_elems: ', len(noslip_elems))
-        print('len of new noslip elems: ', len(new_elems))
-        noslip_elems = new_elems
-
-        # new_noslip_elems = []
-        # for elem in noslip_elems[1:]:
-        #     for node in data["connections"][elem]:
-        #         if data["nodes"][node] not in nodes_in_plane:
-        #             new_noslip_elems += [elem_match]
-        #             break
-
-
-
     #6 times because i have 6 variables
     assembly_left_mat = sym.Matrix(conn_size * 6, conn_size * 6, ([0] * ((conn_size * 6)**2)))
     variables_mat = variables_vector(conn_size)
     assembly_right_mat = sym.Matrix(conn_size * 6, 1, [0] * conn_size * 6)
 
     #output nodes
-    output_nodes = [x for x in data["nodes"] if x.is_output]
+    output_nodes = [x.id for x in data["nodes"] if x.is_output]
     
     #input nodes
-    input_nodes = [x for x in data["nodes"] if x.is_input]     
+    input_nodes = [x.id for x in data["nodes"] if x.is_input]     
 
     #solving each local mat
     for conn in data["connections"]:
@@ -177,7 +123,7 @@ def navier_stokes(data, force_arr):
         local_left_mat = npy.nan_to_num(local_left_mat)
         local_right_mat = npy.nan_to_num(local_right_mat)
 
-        #assembling left side
+        #assembling left and right side
         for i in range(len(conn)):
             for j in range(len(conn)):
                 assembly_left_mat[conn[i] * 6 : (conn[i] * 6) + 6, 
@@ -185,12 +131,60 @@ def navier_stokes(data, force_arr):
                                                                                      j * 6 : j * 6 + 6] 
                 assembly_right_mat[conn[i] * 6 : (conn[i] * 6) + 6, : ] += local_right_mat[i * 6 : (i * 6) + 6, : ] 
 
-    #neumann condition array
-    neumann_position_array = []
-    for i in range(len(data["nodes"])):
-        if data["nodes"][i].is_output:
-            neumann_position_array += [i] 
-   
+    #applying no slip conditions
+    conns_mat = data["connections"]
+    noslip_elems = data["noslip_elems"]
+    noslip_connections = [data["connections"][x] for x in data["noslip_elems"]]
+    noslip_nodes = [x for x in data["nodes"] if x.is_noslip]
+
+    #finding plane and removing nodes that belong to plane (only for no slip nodes)
+    count = 0
+    dirchlet_extra_nodes = [] #velocity = 0 with this ones
+    while len(noslip_elems) > 0:
+        curr_elem = noslip_elems[0]
+        nodes_pos_in_elem = conns_mat[curr_elem]
+
+        #getting the node objects in an array
+        nodes_vals_in_elem = []
+        for i in nodes_pos_in_elem:
+            nodes_vals_in_elem += [data["nodes"][i]]
+
+        #noslip nodes for this iteration (with this i create the plane)
+        curr_noslip_nodes = [x for x in nodes_vals_in_elem if x.is_noslip]
+        plane = sym.Poly(surface_from_three_points(*curr_noslip_nodes[:3]))
+
+        #removing all nodes in plane
+        nodes_in_plane = [x.id for x in data["nodes"] if is_on_plane(x, plane)]
+        normal_vector = sym.Matrix([plane.coeffs()[:3]])
+        if normal_vector.shape[1] < 2:
+            normal_vector.col_insert(1, sym.Matrix([0]))
+            normal_vector.col_insert(2, sym.Matrix([0]))
+        else: 
+            if normal_vector.shape[1] < 3:
+                normal_vector.col_insert(2, sym.Matrix([0]))
+
+        unit_vector = normal_vector / math.sqrt(normal_vector[0, 0]**2 + normal_vector[0, 1]**2 + normal_vector[0, 2]**2)
+        noslip_velocity_vector = unit_vector * 638
+
+        # #removing all no_slip elements that contain this nodes
+        # elem_match = []
+        #
+        # for i, elem in enumerate(noslip_connections):
+        #     for node in elem:
+        #         cond = 0
+        #         for nip in nodes_in_plane:
+        #             cond += data["nodes"][node] == nip
+        #     elem_match.append(cond > 0)
+        #
+        # count += 1
+        #
+        # new_elems = [noslip_elems[x] for x in range(len(noslip_elems)) if not elem_match[x]]
+        dirchlet_extra_nodes += nodes_in_plane
+        noslip_elems = noslip_elems[1:]
+
+
+    dirchlet_extra_nodes = list(set(dirchlet_extra_nodes))
+
     #dirchlet condition array
     dirchlet_position_array = []
     for i in range(len(data["nodes"])):
@@ -207,7 +201,18 @@ def navier_stokes(data, force_arr):
         assembly_right_mat[ : , 0] += assembly_left_mat[ : , dirchlet_position_array[i] * 6] * (-velocity_0)
         assembly_right_mat[ : , 0] += assembly_left_mat[ : , dirchlet_position_array[i] * 6 + 1] * (-velocity_0)
 
+    # for i in range(len(dirchlet_extra_nodes)):
+    #     assembly_right_mat[ : , 0] += assembly_left_mat[ : , dirchlet_extra_nodes[i] * 6] * (-velocity_0) #vx
+    #     assembly_right_mat[ : , 0] += assembly_left_mat[ : , dirchlet_extra_nodes[i] * 6 + 1] * (-velocity_0) #vy
+    #     assembly_right_mat[ : , 0] += assembly_left_mat[ : , dirchlet_extra_nodes[i] * 6 + 1 + 1] * (-velocity_0) #vz
+
+    
     #deleting ecuations solved
+    dirchlet_extra_nodes.sort()
+    dirchlet_position_array += dirchlet_extra_nodes
+    dirchlet_position_array += input_nodes
+    dirchlet_position_array = list(set(dirchlet_position_array))
+    dirchlet_position_array.sort()
     dirchlet_position_array = dirchlet_position_array[::-1]    
 
     rem = sym.Symbol("rem")
@@ -246,26 +251,19 @@ def navier_stokes(data, force_arr):
     cols = [j for j in range(n) if any(assembly_right_mat[i, j] != rem for i in range(m))]
         
     assembly_right_mat = assembly_right_mat[rows, cols]
-
-    #applying no slip conditions
-    conns_mat = data["connections"]
-    noslip_elems = data["noslip_elems"]
-    noslip_nodes = [x for x in data["nodes"] if x.is_noslip]
-
-    #finding plane
-    while len(noslip_nodes) > 0:
-        curr_elem = noslip_elems[0]
-        nodes_pos_in_elem = conns_mat[curr_elem]
-
-        #getting the node objects in an array
-        nodes_vals_in_elem = []
-        for i in nodes_pos_in_elem:
-            nodes_vals_in_elem += [data["nodes"][i]]
-        #noslip nodes for this iteration (with this i create the plane)
-        curr_noslip_nodes = [x for x in nodes_vals_in_elem if x.is_noslip]
-        plane = sym.Poly(surface_from_three_points(*curr_noslip_nodes))
-
-
+ 
+    print('No slip Nodes: \n', len(noslip_nodes))
     print('K Matrix shape: \n', assembly_left_mat.shape)
     print('Vars Matrix shape: \n', variables_mat.shape)
     print('B Matrix shape: \n', assembly_right_mat.shape)
+
+    #solving time 
+    delta_time = data["time_delta"]
+    end_time = data["end_time"]
+    f = sym.lambdify(*variables_mat, variables_mat)
+    for i in range(0, end_time, delta_time):
+        curr_x = f(*[10 for x in range(variables_mat.shape[0])])
+        next_x = (sym.eye(assembly_left_mat.shape[0]) + delta_time * assembly_left_mat).inv() * (curr_x + delta_time * assembly_right_mat)
+        print(next_x)
+        curr_x = next_x
+
